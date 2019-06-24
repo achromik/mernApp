99484c739c66debd738c00a7d08f2a208d7dde52
Aleksander Chromik <achromik@gmail.com>
2019-06-24 11:01:11 +0200

    [573] preapre a login page view

--- frontend/src/components/LoginPage/LoginPage.tsx
+++ frontend/src/components/LoginPage/LoginPage.tsx
@@ -1,25 +1,117 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router';
+import { Link } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { styled } from '@material-ui/styles';
+import pink from '@material-ui/core/colors/pink';
 
+import { sizes } from '@src/config/variables';
 import { AppState } from '@src/config/appState';
 import * as auth from '@src/features/auth/selectors/authenticationSelector';
+import { loginRequest } from '@src/features/auth/actions/authenticationActions';
+import { Credentials } from '@src/features/auth/models/auth';
+
+const AvatarStyled = styled(Avatar)({
+    background: pink[500],
+    marginTop: sizes.marginM,
+    marginBottom: sizes.marginM,
+});
+
+const GridStyled = styled(Grid)({
+    marginTop: sizes.marginM,
+    marginBottom: sizes.marginM,
+});
 
 interface StateProps {
     isAuthenticated: boolean;
+    loginRequest: (credentials: Credentials) => void;
 }
 
-const Login: React.FC<StateProps> = ({ isAuthenticated }: StateProps) => (
-    <>
-        {isAuthenticated && <Redirect to="/profile" />}
-        <div>Login</div>
-    </>
-);
+const Login: React.FC<StateProps> = ({ isAuthenticated, loginRequest }: StateProps) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-const mapStateToProps = (state: AppState): StateProps => ({
+    return (
+        <>
+            {isAuthenticated && <Redirect to="/profile" />}
+            <Container component="main" maxWidth="sm">
+                <GridStyled container direction="column" alignItems="center">
+                    <Grid item xs={12}>
+                        <AvatarStyled>
+                            <LockOutlinedIcon />
+                        </AvatarStyled>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography component="h1" variant="h5">
+                            Sing in
+                        </Typography>
+                    </Grid>
+                </GridStyled>
+                <form
+                    noValidate
+                    onSubmit={e => {
+                        e.preventDefault();
+                        console.log(email, password);
+                        loginRequest({ email, password });
+                    }}
+                >
+                    <GridStyled container spacing={3} justify="center">
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                value={email}
+                                label="Email Address"
+                                type="email"
+                                autoComplete="email"
+                                variant="outlined"
+                                fullWidth
+                                autoFocus
+                                onChange={e => setEmail(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                value={password}
+                                label="Password"
+                                type="password"
+                                autoComplete="current-password"
+                                variant="outlined"
+                                fullWidth
+                                onChange={e => setPassword(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button variant="contained" fullWidth color="primary" type="submit">
+                                Sign in
+                            </Button>
+                        </Grid>
+                    </GridStyled>
+                </form>
+                <Grid container justify="flex-end">
+                    <Grid item>
+                        <Link to="/registration">Don't have an account? Sign Up</Link>
+                    </Grid>
+                </Grid>
+            </Container>
+        </>
+    );
+};
+
+const mapStateToProps = (state: AppState): { isAuthenticated: boolean } => ({
     isAuthenticated: auth.isAuthenticated(state),
 });
 
-const LoginPage = connect(mapStateToProps)(Login);
+const LoginPage = connect(
+    mapStateToProps,
+    { loginRequest },
+)(Login);
 
 export default LoginPage;
--- frontend/src/config/variables.ts
+++ frontend/src/config/variables.ts
@@ -2,3 +2,8 @@ export const colors = {
     spinnerColor: '#2bf',
     spinnerBackgroundColor: 'rgba(0,0,0,0.1)',
 };
+
+export const sizes = {
+    paddingM: '1rem',
+    marginM: '1rem',
+};
