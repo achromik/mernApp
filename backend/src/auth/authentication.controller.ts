3bcb14fbaad7f6324aff865c998ca2d0f829ee26
Aleksander Chromik <achromik@gmail.com>
2019-06-27 17:56:13 +0200

    [573] Add tokens' refreshing

--- backend/src/auth/authentication.controller.ts
+++ backend/src/auth/authentication.controller.ts
@@ -11,13 +11,19 @@ import { InvalidCredentialsException } from '../exceptions/AuthorizationExceptio
 import User from '../users/user.interface';
 import { DataStoredInToken } from '../interfaces/dataStoredInToken.interface';
 import { DataStoredInRefreshToken } from '../interfaces/dataStoredInRefreshToken';
+import { WrongRefreshTokenException } from '../exceptions/WrongRefreshTokenException';
+import refreshTokenModel from './refreshToken.model';
+import { JWTExpiredException } from '../exceptions/JWTExceptions';
 
 export class AuthenticationController implements Controller {
     public readonly path = '/auth';
     public router = express.Router();
     private user = userModel;
+    private refreshToken = refreshTokenModel;
     private secretKey = process.env.JWT_SECRET_KEY as string;
     private refreshSecretKey = process.env.JWT_REFRESH_SECRET_KEY as string;
+    private tokenLifetime = process.env.JWT_TOKEN_LIFETIME;
+    private refreshTokenLifetime = process.env.JWT_REFRESH_TOKEN_LIFETIME;
 
     public constructor() {
         this.initializeRoutes();
@@ -26,11 +32,11 @@ export class AuthenticationController implements Controller {
     private initializeRoutes(): void {
         this.router.post(`${this.path}/register`, this.registration);
         this.router.post(`${this.path}/login`, this.loggingIn);
-        this.router.post(`${this.path}/refreshToken`, this.refreshToken);
+        this.router.post(`${this.path}/refreshToken`, this.refreshingToken);
     }
 
     private createToken = async (user: User): Promise<string> => {
-        const expiresIn = 60 * 5; //5 minutes
+        const expiresIn = this.tokenLifetime;
         const dataStoredInToken: DataStoredInToken = {
             _id: user._id,
             firstName: user.firstName,
@@ -42,7 +48,7 @@ export class AuthenticationController implements Controller {
     };
 
     private createRefreshToken = async (userId: string): Promise<string> => {
-        const expiresIn = '7d';
+        const expiresIn = this.refreshTokenLifetime;
         const dataStoredInToken: DataStoredInRefreshToken = {
             _id: userId,
         };
@@ -101,7 +107,11 @@ export class AuthenticationController implements Controller {
 
                     const refreshToken = await this.createRefreshToken(user._id);
 
-                    await this.user.updateOne({ _id: user._id }, { refreshToken, status: true });
+                    await this.refreshToken.updateOne(
+                        { value: refreshToken },
+                        { value: refreshToken, userId: user._id },
+                        { upsert: true },
+                    );
 
                     res.status(200).json({ token, refreshToken });
                 } else {
@@ -111,15 +121,63 @@ export class AuthenticationController implements Controller {
                 next(new InvalidCredentialsException());
             }
         } catch (err) {
-            next(new InternalErrorException(err.name));
+            next(new InternalErrorException(err.name + ': ' + err.message));
         }
     };
 
-    private refreshToken = async (
+    private refreshingToken = async (
         req: express.Request,
         res: express.Response,
         next: express.NextFunction,
     ) => {
-        next(new Error('Not implemented yet'));
+        try {
+            const storedToken = await this.refreshToken.findOneAndDelete({
+                value: req.body.refreshToken,
+            });
+
+            if (storedToken) {
+                const isTokenMatch = this.compareTokens(storedToken.value, req.body.refreshToken);
+
+                const verifyToken = jwt.verify(
+                    req.body.refreshToken,
+                    this.refreshSecretKey,
+                ) as DataStoredInRefreshToken;
+
+                const user = await this.user.findOne({
+                    _id: verifyToken._id,
+                });
+
+                if (isTokenMatch && user) {
+                    const token = await this.createToken(user);
+                    const refreshToken = await this.createRefreshToken(user._id);
+
+                    await this.refreshToken.updateOne(
+                        { value: refreshToken },
+                        { value: refreshToken, userId: user._id },
+                        { upsert: true },
+                    );
+
+                    res.status(200).send({ token, refreshToken });
+                } else {
+                    next(new WrongRefreshTokenException());
+                }
+            } else {
+                next(new WrongRefreshTokenException());
+            }
+        } catch (err) {
+            next(this.handleRefreshTokenExpiredError(err));
+        }
+    };
+
+    private compareTokens = (firstToken: string, secondToken: string): boolean => {
+        return firstToken === secondToken;
+    };
+
+    private handleRefreshTokenExpiredError = (
+        err: Error,
+    ): InternalErrorException | JWTExpiredException => {
+        return err.name === 'TokenExpiredError'
+            ? new JWTExpiredException()
+            : new InternalErrorException(err.name);
     };
 }
