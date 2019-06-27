4aeeb9afebeb5db2e35afdba1a14ed031601e3eb
Aleksander Chromik <achromik@gmail.com>
2019-06-27 18:29:05 +0200

    [573] Update env validation, remove unused lines of code

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -13,7 +13,6 @@ export class UsersController implements Controller {
     public readonly path = '/users';
     public router = express.Router();
     private secretKey = process.env.JWT_SECRET_KEY as string;
-    private refreshSecretKey = process.env.JWT_REFRESH_SECRET_KEY as string;
     private user = userModel;
 
     public constructor() {
@@ -32,7 +31,7 @@ export class UsersController implements Controller {
         const token = req.headers.authorization as string;
         try {
             const decode = jwt.verify(token, this.secretKey) as { _id: string };
-            console.log(req.user);
+
             const user = await this.user.findById(decode._id);
             if (user) {
                 res.status(200).json(user);
