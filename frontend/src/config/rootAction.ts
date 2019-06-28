14f44238cc81d3fa15d90d475508751e98e55e1a
Aleksander Chromik <achromik@gmail.com>
2019-06-28 21:15:23 +0200

    [579] Add navigation feature to rootEpic and rootActions

--- frontend/src/config/rootAction.ts
+++ frontend/src/config/rootAction.ts
@@ -1,11 +1,12 @@
 import { AuthAction } from '@src/features/auth/actions/authenticationActions';
+import { NavigationAction } from '@src/features/navigation/actions/navigationActions';
 
 export interface Action<T, P = null> {
     readonly type: T;
     readonly payload: P;
 }
 
-export type AppAction = AuthAction;
+export type AppAction = AuthAction | NavigationAction;
 
 export function creator<A extends Action<T, null>, T extends AppAction['type'] = A['type']>(
     type: T,
--- frontend/src/config/rootEpic.ts
+++ frontend/src/config/rootEpic.ts
@@ -1,8 +1,14 @@
 import { combineEpics, Epic as _Epic } from 'redux-observable';
 
+import { AppAction } from './rootAction';
+
 import * as service from './rootService';
 import { authenticationEpicFactory } from '@src/features/auth/epics/authenticationEpic';
+import { navigationRedirectEpicFactory } from '@src/features/navigation/epics/redirectEpics';
 
-export const rootEpic = combineEpics<any>(authenticationEpicFactory(service.userAuthenticator));
+export const rootEpic = combineEpics(
+    authenticationEpicFactory(service.userAuthenticator),
+    navigationRedirectEpicFactory(),
+);
 
-export type Epic = _Epic<any, any>;
+export type Epic = _Epic<AppAction, AppAction>;
