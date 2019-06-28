ab8c0c9c1b2aac6f0d490ba387bd7a4c8c12cd4f
Aleksander Chromik <achromik@gmail.com>
2019-06-28 18:42:47 +0200

    [579] Refactor BackgroundImage component

--- frontend/src/common/components/BackgroundImage.tsx
+++ frontend/src/common/components/BackgroundImage.tsx
@@ -1,20 +1,17 @@
 import * as React from 'react';
-import { styled } from '@material-ui/styles';
-import { Container } from '@material-ui/core';
+import styled from '@emotion/styled';
 
-const BlurredImage = styled(Container)({
-    position: 'absolute',
-    margin: '',
-    padding: 0,
-    height: '110vh',
-    width: '110vw',
-    background: '#00c6ff',
-    filter: 'blur(2px)',
-    backgroundImage:
-        'linear-gradient(90deg, #d53369a3 0%, #daae5145 100%), url("../../assets/images/hero.jpg")',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    zIndex: -1,
-});
+const BlurredImage = styled.div`
+    position: absolute;
+    height: 100vh;
+    width: 100vw;
+    background: #00c6ff;
+    filter: blur(2px);
+    background-image: linear-gradient(90deg, #d53369a3 0%, #daae5145 100%),
+        url('../../assets/images/hero.jpg');
+    background-size: cover;
+    background-position: center;
+    z-index: -1;
+`;
 
-export const BackgroundImage: React.FC = () => <BlurredImage maxWidth={false} />;
+export const BackgroundImage: React.FC = () => <BlurredImage />;
