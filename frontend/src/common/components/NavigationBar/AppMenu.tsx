f594735e5438df91d8c1ef4965bedb876136399b
Aleksander Chromik <achromik@gmail.com>
2019-07-02 21:19:10 +0200

    [590] Refactor menu items rendering

--- frontend/src/common/components/NavigationBar/AppMenu.tsx
+++ frontend/src/common/components/NavigationBar/AppMenu.tsx
@@ -3,12 +3,13 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import IconButton from '@material-ui/core/IconButton';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '@material-ui/core/MenuItem';
+import MenuPosition from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 
 import { isAuthenticated } from '@src/features/auth/selectors/authenticationSelector';
 import { logoutRequest } from '@src/features/auth/actions/authenticationActions';
 import { AppState } from '@src/config/appState';
+import { menuItems, MenuItem } from '@src/config/menuItems';
 
 interface StateProps {
     isAuthenticated: boolean;
@@ -30,30 +31,31 @@ const AppMenuRaw: React.FC<AppMenu> = ({ isAuthenticated, logoutRequest }) => {
         handleMenuClose();
     };
 
+    const renderMenuItem = (item: MenuItem) =>
+        !item.type ? (
+            <MenuPosition key={item.to} onClick={handleMenuClose} component={Link} to={item.to}>
+                {item.text}
+            </MenuPosition>
+        ) : (
+            <MenuPosition key={item.to} onClick={handleLogout} component="div">
+                {item.text}
+            </MenuPosition>
+        );
+
+    const renderMenuItems = (menuItems: MenuItem[]) => {
+        const shouldRenderMenuItem = ({ authenticated }: MenuItem): boolean =>
+            isAuthenticated ? authenticated : !authenticated;
+        const menuItemsToRender = menuItems.filter(shouldRenderMenuItem);
+        return menuItemsToRender.map(renderMenuItem);
+    };
+
     return (
         <div>
             <IconButton color="inherit" onClick={handleMenuOpen}>
                 <AccountCircle />
             </IconButton>
             <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
-                {!isAuthenticated && (
-                    <>
-                        <MenuItem onClick={handleMenuClose} component={Link} to="/profile">
-                            Profile
-                        </MenuItem>
-                        <MenuItem onClick={handleMenuClose} component={Link} to="/login">
-                            Log in
-                        </MenuItem>
-                    </>
-                )}
-                {isAuthenticated && (
-                    <>
-                        <MenuItem onClick={handleMenuClose} component={Link} to="/profile">
-                            Profile
-                        </MenuItem>
-                        <MenuItem onClick={handleLogout}>Log out</MenuItem>
-                    </>
-                )}
+                {renderMenuItems(menuItems)}
             </Menu>
         </div>
     );
