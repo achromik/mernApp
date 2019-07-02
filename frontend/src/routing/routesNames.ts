9075085aa0addbec7c77ae68b29ce28c6015be51
Aleksander Chromik <achromik@gmail.com>
2019-07-02 13:18:20 +0200

    [590] Add routes names

---
+++ frontend/src/routing/routesNames.ts
@@ -0,0 +1,15 @@
+export enum RoutesNames {
+    Home = '/',
+    Login = '/login',
+    SignUp = '/registration',
+    Profile = '/profile',
+    Dashboard = '/dashboard',
+}
+
+export const routesNames = {
+    [RoutesNames.Home]: 'Home',
+    [RoutesNames.Login]: 'Login Page',
+    [RoutesNames.SignUp]: 'Registration Page',
+    [RoutesNames.Profile]: 'Profile',
+    [RoutesNames.Dashboard]: 'Dashboard',
+};
